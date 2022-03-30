7b0205727444dfe57ba0796b960bc3ede783b6cc
Vavasquezh <93843558+Vavasquezh@users.noreply.github.com>
2022-03-29 20:31:35 -0500

    televisores.py

--- Televisores/marca.py
+++
@@ -1,8 +0,0 @@
-class Marca:
-    def __init__(self,marca):
-        self._nombre = marca
-
-    def setNombre(self,marca):
-        self._nombre = marca
-    def getNombre(self):
-        return self._nombre
